14f2a6608b1cc52f8a8dcedeab50bd4cb4fa4ee6
Daniel Chattan <dnchattan@gmail.com>
2021-09-20 17:42:12 -0400

    remove storage name

--- Metadata/Model/Reflection/FieldDescriptor.cs
+++ Metadata/Model/Reflection/FieldDescriptor.cs
@@ -5,19 +5,14 @@ namespace Il2CppToolkit.Model
 {
 	public class FieldDescriptor
 	{
-		private static readonly Regex BackingFieldRegex = new("<(.+)>k__BackingField", RegexOptions.Compiled);
-
 		public FieldDescriptor(string name, ITypeReference typeReference, FieldAttributes attrs, ulong offset)
 		{
-			Name = BackingFieldRegex.Replace(name, match => match.Groups[1].Value);
-			// this is kinda evil, but it will make them consistent in name at least =)
-			StorageName = $"<{Name}>k__BackingField";
+			Name = name;
 			Type = typeReference;
 			Attributes = attrs;
 			Offset = offset;
 		}
 
-		public readonly string StorageName;
 		public readonly string Name;
 		public readonly ITypeReference Type;
 		public FieldAttributes Attributes;
