ef973fa2c0801b21767c6bd6c0330a7e3214ab02
Daniel Chattan <dnchattan@gmail.com>
2021-09-26 09:59:14 -0400

    add program.cs with config options

---
+++ ReverseCompiler.Cli/Program.cs
@@ -0,0 +1,29 @@
+
+using Il2CppToolkit.Model;
+
+namespace Il2CppToolkit.ReverseCompiler.Cli
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    args = System.IO.File.ReadAllLines(@".\test.config");
+                }
+            }
+            catch { }
+            Loader loader = new Loader();
+            loader.Init(
+                args[0],
+                args[1]
+                );
+            TypeModel model = new TypeModel(loader);
+            AssemblyGenerator asmGen = new AssemblyGenerator(model);
+            asmGen.TypeSelectors.Add(td => td.Name == args[2]);
+            asmGen.Execute().Wait();
+        }
+    }
+}
