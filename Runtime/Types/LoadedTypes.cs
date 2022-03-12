50cea57cd32209d5846adc7a9ccae8cc3ea0696f
Daniel Chattan <dnchattan@gmail.com>
2022-03-11 23:59:50 -0500

    revert LoadedTypes change

--- Runtime/Types/LoadedTypes.cs
+++ Runtime/Types/LoadedTypes.cs
@@ -1,21 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using Il2CppToolkit.Common;
-using Il2CppToolkit.Common.Errors;
 using Il2CppToolkit.Runtime.Types.Reflection;
 
 namespace Il2CppToolkit.Runtime.Types
 {
     public static class LoadedTypes
     {
-        private static readonly Dictionary<string, Type> s_tokenToType = new();
+        private static readonly Dictionary<string, Type> s_nameToType = new();
 
         public static Type GetType(ClassDefinition classDef)
         {
-            var classType = classDef.byval_arg.TypeHandle;
-            string token = Utilities.GetTypeTag(classType._nameIndex, classType._namespaceIndex, classDef.Token);
-            return s_tokenToType.TryGetValue(token, out Type retVal) ? retVal : null;
+            string className = classDef.FullName;
+            return s_nameToType.TryGetValue(classDef.FullName, out Type retVal) ? retVal : null;
         }
 
         static LoadedTypes()
@@ -29,7 +26,7 @@ namespace Il2CppToolkit.Runtime.Types
                     TagAttribute attr = type.GetCustomAttribute<TagAttribute>();
                     if (attr == null) continue;
 
-                    ErrorHandler.Assert(s_tokenToType.TryAdd(attr.Tag, type), "Duplicate type by token");
+                    s_nameToType.Add(type.FullName, type);
                 }
             }
         }
