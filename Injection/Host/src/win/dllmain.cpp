a3225f23f6581cd975f08507105c09a530e1509b
Daniel Chattan <dnchattan@gmail.com>
2022-06-10 23:11:14 -0400

    don't hook hookers

--- Injection/Host/src/win/dllmain.cpp
+++ Injection/Host/src/win/dllmain.cpp
@@ -7,28 +7,38 @@
 #include "WindowHelpers.h"
 #include "../MessageHandler.h"
 
+bool IsUnityProcess() noexcept
+{
+	HMODULE hUnityPlayer{ GetModuleHandleA("UnityPlayer.dll") };
+	return !!hUnityPlayer;
+}
+
+#define CHECKED_CALL(method, params) if ((mhStatus = method params ) != MH_STATUS::MH_OK) { \
+	OutputDebugStringA("ERROR: " #method " returned an error:"); \
+	OutputDebugStringA(MH_StatusToString(mhStatus)); \
+	return TRUE; \
+} \
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
+	if (!IsUnityProcess())
+		return TRUE;
+
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH:
-		{
-			if (MH_Initialize() != MH_OK)
-			{
-				// TODO: Handle error
-			}
-			if (MH_CreateHook(&PeekMessageA, &PeekMessage_Injected, reinterpret_cast<LPVOID*>(&pfnPeekMessage)) != MH_OK)
-			{
-				// TODO: Handle error
-			}
-			MH_EnableHook(PeekMessageA);
-			return TRUE;
-		}
-	case DLL_PROCESS_DETACH:
-		if (MH_Uninitialize() != MH_OK)
-		{
-			// TODO: Handle error
-		}
+	{
+		MH_STATUS mhStatus{ MH_STATUS::MH_OK };
+		CHECKED_CALL(MH_Initialize, ());
+		CHECKED_CALL(MH_CreateHook, (&PeekMessageA, &PeekMessage_Injected, reinterpret_cast<LPVOID*>(&pfnPeekMessage)));
+		CHECKED_CALL(MH_EnableHook, (&PeekMessageA));
 		return TRUE;
+	}
+	case DLL_PROCESS_DETACH:
+	{
+		MH_STATUS mhStatus{ MH_STATUS::MH_OK };
+		CHECKED_CALL(MH_Uninitialize, ());
+		return TRUE;
+	}
 	case DLL_THREAD_ATTACH:
 		return TRUE;
 	case DLL_THREAD_DETACH:
