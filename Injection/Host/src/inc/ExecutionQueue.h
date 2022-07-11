0200bc55707d118a4cb2bcb167d37654cca7cf3d
Daniel Chattan <dnchattan@gmail.com>
2022-07-10 23:03:50 -0400

    rest of fix to splinlock

--- Injection/Host/src/inc/ExecutionQueue.h
+++ Injection/Host/src/inc/ExecutionQueue.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <functional>
 #include <optional>
+#include <atomic>
 #include "safe_queue.h"
 
 class ExecutionQueue
@@ -10,38 +11,40 @@ public:
 	void Shutdown() noexcept
 	{
 		q.clear();
-		isShutdown = true;
+		isShutdown.store(true);
 	}
 
 	template<typename T>
 	std::optional<T> Invoke(std::function<T()>&& task) noexcept
 	{
-		bool finished{ false };
-		std::optional<T> result{ std::nullopt };
+		std::atomic<bool> finished{false};
+		std::optional<T> result{std::nullopt};
 
-		if (isShutdown)
+		if (isShutdown.load())
 			return result;
 
-		q.enqueue([task = std::move(task), &result, &finished]() mutable noexcept
-		{
+		q.enqueue([task = std::move(task), &result, &finished]() mutable noexcept {
 			result = task();
-			finished = true;
+			finished.store(true);
 		});
-		
-		while (!finished && !isShutdown);
+
+		while (!finished.load() && !isShutdown.load())
+			this_thread::yield();
 
 		return result;
 	}
 
 	void DoWork() noexcept
 	{
-		if (isShutdown)
+		if (isShutdown.load())
 			return;
 
 		std::function<void()> fn;
-		while(q.try_dequeue(fn)) fn();
+		while (q.try_dequeue(fn))
+			fn();
 	}
+
 private:
-	bool isShutdown{ false };
+	std::atomic<bool> isShutdown{false};
 	safe_queue<std::function<void()>> q;
 };
