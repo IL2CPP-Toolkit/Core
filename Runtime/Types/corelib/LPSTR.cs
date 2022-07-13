3e705be3d4488e4371bd196a59006e7020463e9c
Daniel Chattan <dnchattan@gmail.com>
2022-07-13 00:09:35 -0400

    add missing lpcstr ctors

--- Runtime/Types/corelib/LPSTR.cs
+++ Runtime/Types/corelib/LPSTR.cs
@@ -3,25 +3,28 @@ using System.Text;
 
 namespace Il2CppToolkit.Runtime.Types.corelib
 {
-    public class Native__LPSTR : RuntimeObject
-    {
-        private string m_value;
-        public string Value
-        {
-            get
-            {
-                if (m_value == null)
-                {
-                    ulong address = Source.ReadPointer(Address);
-                    ReadOnlyMemory<byte> stringData = Source.ReadMemory(Address, 512);
+	public class Native__LPSTR : RuntimeObject
+	{
+		public Native__LPSTR() : base() { }
+		public Native__LPSTR(IMemorySource source, ulong address) : base(source, address) { }
+
+		private string m_value;
+		public string Value
+		{
+			get
+			{
+				if (m_value == null)
+				{
+					ulong address = Source.ReadPointer(Address);
+					ReadOnlyMemory<byte> stringData = Source.ReadMemory(Address, 512);
 #if NET472
-                    m_value = Encoding.UTF8.GetString(stringData.Span.ToArray()).Split(new char[] { '\0' }, 2)[0];
+					m_value = Encoding.UTF8.GetString(stringData.Span.ToArray()).Split(new char[] { '\0' }, 2)[0];
 #else
                     m_value = Encoding.UTF8.GetString(stringData.Span).Split(new char[] { '\0' }, 2)[0];
 #endif
-                }
-                return m_value;
-            }
-        }
-    }
+				}
+				return m_value;
+			}
+		}
+	}
 }
