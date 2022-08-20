933218313268fcd3adea6a0b29ab79daf0c9d6ad
Daniel Chattan <dnchattan@gmail.com>
2022-08-20 00:51:49 -0400

    fix nullables :(

--- Runtime/Types/corelib/Nullable.cs
+++ Runtime/Types/corelib/Nullable.cs
@@ -2,20 +2,20 @@
 
 namespace Il2CppToolkit.Runtime.Types.corelib
 {
-    [TypeFactory(typeof(Nullable<>))]
-    public class NullableFactory<T> : ITypeFactory where T : struct
-    {
-        public object ReadValue(IMemorySource source, ulong address)
-        {
-            UnknownObject obj = new(source, address);
-            bool hasValue = Il2CppTypeInfoLookup<Nullable<T>>.GetValue<bool>(obj, "has_value");
-            T value = hasValue ? Il2CppTypeInfoLookup<Nullable<T>>.GetValue<T>(obj, "value") : default;
-            return hasValue ? new Nullable<T>() : new Nullable<T>(value);
-        }
+	[TypeFactory(typeof(Nullable<>))]
+	public class NullableFactory<T> : ITypeFactory where T : struct
+	{
+		public object ReadValue(IMemorySource source, ulong address)
+		{
+			UnknownObject obj = new(source, address);
+			bool hasValue = Il2CppTypeInfoLookup<Nullable<T>>.GetValue<bool>(obj, "has_value");
+			T value = hasValue ? Il2CppTypeInfoLookup<Nullable<T>>.GetValue<T>(obj, "value") : default;
+			return hasValue ? new Nullable<T>(value) : new Nullable<T>();
+		}
 
-        public void WriteValue(IMemorySource source, ulong address, object value)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public void WriteValue(IMemorySource source, ulong address, object value)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
