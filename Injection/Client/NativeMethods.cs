2aa8f6b27fddb4ac0180893fb9e907e7b1e3e081
Daniel Chattan <dnchattan@gmail.com>
2023-11-20 21:27:04 -0500

    avoid file conflicts for installed dlls

--- Injection/Client/NativeMethods.cs
+++ Injection/Client/NativeMethods.cs
@@ -1,19 +1,33 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Il2CppToolkit.Injection.Client
 {
 	internal static class NativeMethods
 	{
+		static NativeMethods()
+		{
+			try
+			{
+				// make a copy so the installed version does not have a lock
+				string sourceDll = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Il2CppToolkit.Injection.Host.dll");
+				string workingDll = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "$Il2CppToolkit.Injection.Host.dll");
+				File.Copy(sourceDll, workingDll, true);
+				File.SetAttributes(workingDll, FileAttributes.Hidden);
+			}
+			catch { }
+		}
 		public struct PublicState
 		{
 			public int port;
 		}
 
-		[DllImport("Il2CppToolkit.Injection.Host.dll")]
+		[DllImport("$Il2CppToolkit.Injection.Host.dll")]
 		public static extern int InjectHook(uint dwProcId);
-		[DllImport("Il2CppToolkit.Injection.Host.dll")]
+		[DllImport("$Il2CppToolkit.Injection.Host.dll")]
 		public static extern int ReleaseHook(uint dwProcId);
-		[DllImport("Il2CppToolkit.Injection.Host.dll")]
+		[DllImport("$Il2CppToolkit.Injection.Host.dll")]
 		public static extern int GetState(uint dwProcId, ref PublicState state, int dwTimeoutMs = 3000);
 	}
 }
